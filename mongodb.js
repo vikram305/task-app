d2fa3fce9884db29f93ee42c03c65300ca056164
vikram <mauryavikram305@gmail.com>
2020-11-12 09:32:39 +0530

    Updating documents

--- mongodb.js
+++ mongodb.js
@@ -16,41 +16,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
     console.log(`client db name:  ${client.db.name}`)
 
-    // const updatePromise=db.collection('users').updateOne({
-    //     _id: new ObjectID("5faa8a72efe79942bc1b738c")
-    // }, {
-    //     $set:{
-    //         name: 'JHK'
-    //     }
-    // })
-
-    // updatePromise.then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-
-    // db.collection('users').updateOne({
-    //     _id: new ObjectID("5faa8b2c28296012b4e97561")
-    // }, {
-    //     $set:{
-    //         name: 'ZYH'
-    //     }
-    // }).then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set:{
-            completed: true
-        }
-    }).then((result) => {
+    db.collection('users').deleteMany({
+       age:20 
+    }).then((result)=>{
         console.log(result)
-    }).catch((error) => {
+    }).catch((error)=>{
         console.log(error)
     })
 })
