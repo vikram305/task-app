24ab8eb72af9a2588579fba4e2259c47d250c8a3
vikram305 <mauryavikram305@gmail.com>
2020-11-16 09:27:29 +0530

    added data sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose =  require('mongoose')
+const validator = require('validator')
 
 const databaseName='task-manager'
 const connectionURL=`mongodb+srv://vikram305:vikram305@learningprojects.1zeq7.mongodb.net/${databaseName}?retryWrites=true&w=majority`
@@ -9,17 +10,50 @@ mongoose.connect(connectionURL, {
 
 const User = mongoose.model('Users', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        require: true,
+        trim: true,
+        minlength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password can not contains "password"')
+            }
+        }
     },
     
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 // const me = new User({
-//     name: 'ABC',
-//     age:'Mike'
+//     name: '   DEF   ',
+//     email: '  def@gmail.com  ',
+//     password: 'def123@'
 // })
 
 // me.save().then(()=>{
@@ -30,18 +64,19 @@ const User = mongoose.model('Users', {
 
 const Task = mongoose.model('task',{
     description: {
-
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
-    description: 'Task1',
-    completed: false
+    description: 'Task2',
 })
 
 task.save().then(() => {
