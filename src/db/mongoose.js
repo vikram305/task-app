039761f9fefce6bcbe61f87d6138fddfb75bd84f
vikram305 <mauryavikram305@gmail.com>
2020-11-16 10:14:58 +0530

    Resource Creation Endpoint 1 (User Creation)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose =  require('mongoose')
-const validator = require('validator')
 
 const databaseName='task-manager'
 const connectionURL=`mongodb+srv://vikram305:vikram305@learningprojects.1zeq7.mongodb.net/${databaseName}?retryWrites=true&w=majority`
@@ -7,60 +6,6 @@ mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
 })
-
-const User = mongoose.model('Users', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-
-    password: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password can not contains "password"')
-            }
-        }
-    },
-    
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '   DEF   ',
-//     email: '  def@gmail.com  ',
-//     password: 'def123@'
-// })
-
-// me.save().then(()=>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log(`Error: ${error}`)
-// })
 
 const Task = mongoose.model('task',{
     description: {
@@ -73,14 +18,4 @@ const Task = mongoose.model('task',{
         type: Boolean,
         default: false
     }
-})
-
-const task = new Task({
-    description: 'Task2',
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
 })
