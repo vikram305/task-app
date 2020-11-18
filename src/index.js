03725d8a97b264ddc8ee7a0752f9192a7e55c9e3
vikram305 <mauryavikram305@gmail.com>
2020-11-18 09:59:36 +0530

    Resource Reading Endpoints

--- src/index.js
+++ src/index.js
@@ -20,6 +20,28 @@ app.post('/users', (req,res) => {
    })
 })
 
+app.get('/users', (req,res) => {
+     User.find({}).then((users) => {
+          res.send(users)
+     }).catch((error) => {
+          res.status(500).send(error)
+     })
+})
+
+app.get('/users/:id', (req,res) => {
+     const id=req.params.id
+     User.findById(id).then((user) => {
+          if(!user){
+               return res.status(404).send()
+          }
+
+          res.send(user)
+     }).catch((error) => {
+          res.status(500).send()
+     })
+     console.log(req.params)
+})
+
 app.post('/tasks', (req,res) => {
      const task = new Task(req.body)
      
@@ -27,6 +49,27 @@ app.post('/tasks', (req,res) => {
           res.status(201).send(task)
      }).catch((error) => {
           res.status(400).send(error)
+     })
+})
+
+app.get('/tasks', (req,res) => {
+     Task.find({}).then((tasks) => {
+          res.send(tasks)
+     }).catch((error) => {
+          res.status(500).send(error)
+     })
+})
+
+app.get('/tasks/:id', (req,res) => {
+     const id = req.params.id
+     Task.findById(id).then((task) => {
+          if(!task){
+               return res.status(404).send()
+          }
+
+          res.send(task)
+     }).catch((error) => {
+          res.status(500).send(error)
      })
 })
 
