03329f2cbfb9c547471f73a456c37c06e17b696d
vikram305 <mauryavikram305@gmail.com>
2020-11-19 18:00:06 +0530

    Logging in User

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,6 +11,15 @@ router.post('/users', async (req,res) => {
       res.status(400).send(error)
     }
  })
+
+ router.post('/users/login', async(req,res) => {
+      try{
+          const user = await User.findByCredentials(req.body.email, req.body.password)
+          res.send(user)
+      } catch(error){
+          res.status(400).send()
+      }
+ })
  
  router.get('/users', async (req,res) => {
  
