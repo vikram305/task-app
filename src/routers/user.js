f3c727fc60e0070fcabd5aceefb6cab2cf6792b8
vikram305 <mauryavikram305@gmail.com>
2020-11-23 09:49:16 +0530

    Image Formatting

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 
 router.post('/users', async (req,res) => {
@@ -123,7 +124,9 @@ router.post('/users', async (req,res) => {
  })
 
  router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
-      req.user.avatar = req.file.buffer
+
+     const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+      req.user.avatar = buffer
       await req.user.save()
       res.send()
  }, (error, req, res, next) => {
@@ -145,7 +148,7 @@ router.post('/users', async (req,res) => {
                throw new Error()
           }
 
-          res.set('Content-Type', 'image/jpg')
+          res.set('Content-Type', 'image/png')
           res.send(user.avatar)
       } catch(error) {
            res.status(404).send()
